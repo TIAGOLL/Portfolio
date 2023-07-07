a4a4503489c0fad82c0cd91dcc9b22f15a5a0584
Tiago <tiagoepitanga10@gmail.com>
2023-07-07 13:09:32 -0300

    Initial configs

---
+++ src/pages/main.tsx
@@ -0,0 +1,10 @@
+
+const Main = () => {
+    return (
+        <>
+            <h1>Hello Word</h1>
+        </>
+    )
+} 
+
+export default Main;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors:
+      {
+        'main-color': '#506ee5',
+        'secondary-color': '#7037cd',
+        'bg-color': '#d9d9d9',
       },
     },
+    variants: {},
+    plugins: [],
   },
   plugins: [],
 }
